f7a67daa5cd6cffd85a2b8160131390dcf949c29
BerkutRuslan <berkutruslan1@gmail.com>
2019-09-09 19:00:58 +0300

    added new files, fixed some errors

--- Shop/admin.py
+++ Shop/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from Shop.models import Game
 
-admin.site.register(Game)
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'id')
+    list_display_links = ('title',)
+
+
+admin.site.register(Game, GameAdmin)
