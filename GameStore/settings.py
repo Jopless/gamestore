13a5aeaf8b4598fcb610794088b063bab6a72a6c
BerkutRuslan <berkutruslan1@gmail.com>
2019-09-11 16:39:49 +0300

    added accounts app in installed apps

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',  # added accounts app
     'Shop.apps.ShopConfig',  # added shop app
     'django.contrib.admin',
     'django.contrib.auth',
