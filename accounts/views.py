7595e27455ae093c64c58f2c0db048764c17c42c
BerkutRuslan <berkutruslan1@gmail.com>
2019-09-13 20:21:57 +0300

    created views for register, login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # Check if passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                # Check email
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    # looks good
+                    user = User.objects.create_user(username=username, password=password, email=email)
+                    # Login after register
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+
