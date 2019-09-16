5d88451d597d78f849756ff87f3e7ce28f63cb59
BerkutRuslan <berkutruslan1@gmail.com>
2019-09-16 14:42:02 +0300

    created logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from accounts import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
