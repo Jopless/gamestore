54ea14b3b55850a9b5c57b4cebdcbc7a91b156a6
BerkutRuslan <berkutruslan1@gmail.com>
2019-09-17 19:31:36 +0300

    added contact me form

---
+++ contacts/views.py
@@ -0,0 +1,25 @@
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+
+def view_email(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['jopless11@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "contacts/email.html", {'form': form})
+
+
+def view_success(request):
+    return render(request, "contacts/success.html")
