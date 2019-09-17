1df9cd6b67e8b09ece7dbeee2337e553ad93d40e
BerkutRuslan <berkutruslan1@gmail.com>
2019-09-17 22:50:51 +0300

    added links url

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from contacts import views
 from .views import view_email, view_success
 
 urlpatterns = [
+    path('links/', views.socbutton, name='links'),
     path('email/', view_email, name='email'),
     path('success/', view_success, name='success'),
 ]
