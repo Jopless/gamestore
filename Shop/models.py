3229429ac0b513eb28f2ccaecd87ca1cb5e60263
Artem <akatrek@gmail.com>
2019-09-18 16:24:57 +0300

    Added comments section for games (now only from admin panel)

--- Shop/models.py
+++ Shop/models.py
@@ -36,3 +36,18 @@ class Game(models.Model):
     def __str__(self):
 
         return self.title
+
+class Comments(models.Model):
+    post = models.ForeignKey('Shop.Game', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
