538e6533615cd72ae845b38b8b083611713218c8
BerkutRuslan <berkutruslan1@gmail.com>
2019-09-19 15:43:04 +0300

    changed debug value

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', default=True)
 
 ALLOWED_HOSTS = []
 
